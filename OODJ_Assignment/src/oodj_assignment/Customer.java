683525f38707d0a7d9cde70d8f9ef6ecad239489
ctk0109 <dialga12123@gmail.com>
2021-08-25 22:57:33 +0800

    Login Done
    login done

--- OODJ_Assignment/src/oodj_assignment/Customer.java
+++ OODJ_Assignment/src/oodj_assignment/Customer.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 package oodj_assignment;
+import java.util.*;
+import java.io.*;
+import javax.swing.*;
+import javax.swing.JFrame;
 
 /**
  *
@@ -19,6 +23,63 @@ public class Customer
     protected String phoneNumber;
     protected String address;
     
+    static JFrame errorMessage;
+    public static String login(String usr, String pwd)
+    {
+        try
+        {
+            //compare username with all the username in the text file
+            BufferedReader br = new BufferedReader(new FileReader("src\\oodj_assignment\\textFile\\Users.txt"));
+            String line;
+            Boolean flagUsrname = false;
+            Boolean flagPwd = false;
+            String userType = "";
+            
+            while ((line = br.readLine()) != null)
+            {
+                String [] user = line.split(",",7); //splits user detail by commas
+                
+                if (usr.equals(user[1]))
+                {
+                    flagUsrname = true;
+                    
+                    if (pwd.equals(user[2]))
+                    {
+                        flagPwd = true;
+
+                    }
+                    userType = user[0].substring(0,3);
+                }
+                
+            }
+            br.close();
+
+            if (flagUsrname == true && flagPwd == true)
+            {
+                System.out.println(userType);
+                if (userType.equals("ADM"))
+                {
+                    return "adm";
+                }
+                else if (userType.equals("CUS"))
+                {
+                    return "cus";
+                }
+                        
+            }
+            else
+                {
+                    JOptionPane.showMessageDialog(errorMessage, "Incorrect Username or Password! Please try again.","Error",JOptionPane.WARNING_MESSAGE);
+                }
+            
+
+        }
+        catch(IOException Ex)
+        {
+            JOptionPane.showMessageDialog(errorMessage, " An Error Occured. Please try again","Error",JOptionPane.WARNING_MESSAGE);
+        }
+        return "null";
+    }
     public static void addUser()
     {
         
--- OODJ_Assignment/src/oodj_assignment/loginMenu.java
+++ OODJ_Assignment/src/oodj_assignment/loginMenu.java
@@ -5,6 +5,8 @@
  */
 package oodj_assignment;
 
+import java.awt.CardLayout;
+
 /**
  *
  * @author Asus
@@ -32,10 +34,10 @@ public class loginMenu extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
+        txtLoginUsrname = new javax.swing.JTextField();
+        txtLoginPwd = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        btnLogin = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jLabel6 = new javax.swing.JLabel();
 
@@ -63,8 +65,13 @@ public class loginMenu extends javax.swing.JFrame {
         jLabel5.setForeground(new java.awt.Color(204, 51, 0));
         jLabel5.setText("Incorrect password!");
 
-        jButton1.setFont(new java.awt.Font("Segoe UI", 3, 24)); // NOI18N
-        jButton1.setText("Enter");
+        btnLogin.setFont(new java.awt.Font("Segoe UI", 3, 24)); // NOI18N
+        btnLogin.setText("Enter");
+        btnLogin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLoginActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Register");
 
@@ -81,7 +88,7 @@ public class loginMenu extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(307, 307, 307)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel5)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(192, 192, 192)
@@ -91,10 +98,10 @@ public class loginMenu extends javax.swing.JFrame {
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGap(18, 18, 18)
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 233, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(txtLoginUsrname, javax.swing.GroupLayout.PREFERRED_SIZE, 233, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                                 .addGap(18, 18, 18)
-                                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 233, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                .addComponent(txtLoginPwd, javax.swing.GroupLayout.PREFERRED_SIZE, 233, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
@@ -124,15 +131,15 @@ public class loginMenu extends javax.swing.JFrame {
                 .addGap(21, 21, 21)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtLoginUsrname, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(19, 19, 19)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtLoginPwd, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(jLabel5)
                 .addGap(45, 45, 45)
-                .addComponent(jButton1)
+                .addComponent(btnLogin)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 66, Short.MAX_VALUE)
                 .addComponent(jLabel6)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -153,6 +160,26 @@ public class loginMenu extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
+        // Get login credentials
+        String username = txtLoginUsrname.getText();
+        String pwd = txtLoginPwd.getText();
+    
+        String userType = Customer.login(username,pwd);
+        if (userType == "adm")
+        {
+            Admin_Menu admMenu = new Admin_Menu();
+            this.setVisible(false);
+            admMenu.setVisible(true);
+        }
+        else if (userType == "cus")
+        {
+            Cus_Menu cusMenu = new Cus_Menu();
+            this.setVisible(false);
+            cusMenu.setVisible(true);
+        }
+    }//GEN-LAST:event_btnLoginActionPerformed
 
     /**
      * @param args the command line arguments
@@ -190,7 +217,7 @@ public class loginMenu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btnLogin;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -199,7 +226,7 @@ public class loginMenu extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField txtLoginPwd;
+    private javax.swing.JTextField txtLoginUsrname;
     // End of variables declaration//GEN-END:variables
 }

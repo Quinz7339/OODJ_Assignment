89f04e65a0a5f94476044479ba5c0471cfa5a98e
Quinz7339 <philipchua7339@gmail.com>
2021-08-30 02:15:13 +0800

    comments to help with viewOrder

--- OODJ_Assignment/src/oodj_assignment/Customer.java
+++ OODJ_Assignment/src/oodj_assignment/Customer.java
@@ -240,13 +240,24 @@ public class Customer
         
         
         Admin adm = new Admin();
-        prodList = adm.viewProduct();
+        prodList = adm.viewProduct(); //return the contents of Products.txt
         
         
         ArrayList<String> tempOrderIDDup = new ArrayList();
         ArrayList<String> tempOrderIDNoDup = new ArrayList();
         
         //retrieve order and get its order item's details with the product list
+        
+        //todo: 1. take in every product, 
+        //2. create a list of unique order id that is only specific to the customer
+        //3. for each orderid, if UID matches, create a temp <orderitem> arrlist to store of orderitems (specific to uid and order item)
+        //4. 3 for each orderid, create temp arraylist of orderItems
+        //4 for each product, if uid matches, 
+        //5 create new orderitem, then append to your temp arraylist 
+        //6 end for, create order(orderid, tempArrayList), then append to master list of orders 
+        // 7 end for, 
+        //8 return masterlist
+        
         try
         {
             //ArrayList to retrieve  objects
